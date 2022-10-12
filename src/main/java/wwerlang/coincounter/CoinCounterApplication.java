427533ed974d0ddbe28c5a8572081bfbe9bee1e2
Willian Werlang <willianwerlang@hotmail.com>
2022-10-11 21:23:45 -0300

    Initial project from Spring Initializr

---
+++ src/main/java/wwerlang/coincounter/CoinCounterApplication.java
@@ -0,0 +1,13 @@
+package wwerlang.coincounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wwerlang/coincounter/CoinCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package wwerlang.coincounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
